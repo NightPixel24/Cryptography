de418d2c3416e15b16a36e120f866bddb8f652b0
Josh <52042379+NightPixel24@users.noreply.github.com>
2022-08-07 22:45:57 +0800

    Indent Fixing

--- Affine/ValidKey.java
+++ Affine/ValidKey.java
@@ -19,45 +19,43 @@
      public static boolean valid(int a, int b) 
      {
          boolean validKey = true;
-	     int compare[] = {3,9,27};
+	 int compare[] = {3,9,27};
 
-	     if((a < 0) || (a > 26))
-	     {
-	         System.out.println("INVALID KEY A:"+ a + "\n");
-	         validKey = false;
-	     }
+	 if((a < 0) || (a > 26))
+	 {
+	     System.out.println("INVALID KEY A:"+ a + "\n");
+	     validKey = false;
+	 }
          else if((b < 0) || (b > 26)) 
          {
-	         System.out.println("INVALID KEY B:"+ b + "\n");
-	         validKey = false;
-	     }
-	     else
+	     System.out.println("INVALID KEY B:"+ b + "\n");
+	     validKey = false;
+	 }
+	 else
          {
              //FACTORISE A AND COMPARE CO-PRIME TO 27
-	         for(int ii = 1; ii <= a; ii++)
+	     for(int ii = 1; ii <= a; ii++)
              {   
-		         //NUMBER IS A FACTOR
+		 //NUMBER IS A FACTOR
                  if(a % ii == 0)
-	             {
-             
-                     //COMPARE TO 26 FACTORS
-		             for(int jj = 0; jj < compare.length; jj ++)
-		             {
-                         if(ii == compare[jj]) //COMPARE TO FACTORS IN ARRAY
-		                 {
-			                 //CO-PRIME
-			                 validKey = false;
-			             }
-		             }     
-                 }
-	         }
-	         if(validKey == false)
 	         {
-                 System.out.println("INVALID KEY A IS NOT CO PRIME TO 26\n");
-	         }
+                      //COMPARE TO 26 FACTORS
+		      for(int jj = 0; jj < compare.length; jj ++)
+		      {
+                          if(ii == compare[jj]) //COMPARE TO FACTORS IN ARRAY
+		          {
+			      //CO-PRIME
+			      validKey = false;
+			  }
+		      }     
+                 }
 	     }
-        
-	     return validKey; 
+	     if(validKey == false)
+	     {
+                 System.out.println("INVALID KEY A IS NOT CO PRIME TO 26\n");
+	     }
+	 }
+	 return validKey; 
      }
 
  }
